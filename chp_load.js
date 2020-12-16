0c40871f76b06777b43b9c8a99a000f77f8f693e
Basar <74671944+Basarus@users.noreply.github.com>
2020-12-16 17:04:22 +0300

    Add files via upload

--- chp_load.js
+++ chp_load.js
@@ -3,7 +3,7 @@ import * as native from 'natives';
 
 let player = alt.Player.local;
 let pos = { x: 4895.28, y: -5744.58, z: 26.351 };
-loaded = false;
+let loaded = false;
 
 alt.setInterval(load_island, 1000)
 
